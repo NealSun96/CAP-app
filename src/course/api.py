739bcc0f7c068d7926086b177c6baecfc89db90d
Neal Sun <nealsun@Neals-MacBook-Pro.local>
2017-01-02 21:53:19 -0500

    whats going on

--- src/course/api.py
+++ src/course/api.py
@@ -28,7 +28,7 @@ class CourseResource(CorsResourceBase, ModelResource):
         return [
             url(r"^(?P<resource_name>%s)/add_course%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('add_course'), name="api_add_course"),
             url(r"^(?P<resource_name>%s)/edit_course/(?P<id>\d+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('edit_course'), name="api_edit_course"),
-            url(r"^(?P<resource_name>%s)/get_assignments/(?P<id>\d+)/(?P<type>\w+)/(?P<level>\w+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_assignments'), name="api_get_assignments"),
+            url(r"^(?P<resource_name>%s)/get_assignments/(?P<id>\d+)/(?P<type>\w+)/(?P<level>[\w-]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_assignments'), name="api_get_assignments"),
         ]
 
     def authorized_read_list(self, object_list, bundle):
@@ -88,26 +88,6 @@ class CourseResource(CorsResourceBase, ModelResource):
     def get_assignments(self, request, **kwargs):
         self.method_check(request, allowed=['get'])
         self.is_authenticated(request)
-        user_group = request.user.groups.first().name
-        if user_group != "teacher":
-            raise ImmediateHttpResponse(HttpBadRequest('Not a teacher'))
-
-        deserialized = self.deserialize(request, request.body,
-                                        format=request.META.get('CONTENT_TYPE', 'application/json'))
-        deserialized = self.alter_deserialized_detail_data(request, deserialized)
-        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
-
-        Course(course_name=bundle.data.get('course_name'), start_time=bundle.data.get('start_time'), teacher=bundle.request.user).save()
-
-        return self.create_response(request, {})
-
-    def edit_course(self, request, **kwargs):
-        self.method_check(request, allowed=['post'])
-        self.is_authenticated(request)
-
-        user_group = request.user.groups.first().name
-        if user_group != "teacher":
-            raise ImmediateHttpResponse(HttpBadRequest('Not a teacher'))
 
         try:
             course = Course.objects.get(id=kwargs['id'], teacher=request.user)
@@ -147,24 +127,8 @@ class CourseResource(CorsResourceBase, ModelResource):
             } for question in knowledge_test.questionordered_set.all()]
             objects.append(bundle)
 
-        deserialized = self.deserialize(request, request.body,
-                                        format=request.META.get('CONTENT_TYPE', 'application/json'))
-        deserialized = self.alter_deserialized_detail_data(request, deserialized)
-        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
+        object_list = {
+            'objects': objects,
+        }
 
-        if bundle.data.get('teacher') != bundle.request.user.username:
-            try:
-                other_teacher = User.objects.get(username=bundle.data.get('teacher'))
-                other_teacher_group = other_teacher.groups.first().name
-                if other_teacher_group != "teacher":
-                    raise ImmediateHttpResponse(HttpBadRequest('Assigned teacher is not a teacher'))
-            except ObjectDoesNotExist:
-                raise ImmediateHttpResponse(HttpNotFound('Assigned teacher does not exist'))
-            course.teacher = other_teacher
-
-        course.course_name = bundle.data.get('course_name')
-        course.start_time=bundle.data.get('start_time')
-        course.done = bundle.data.get('done')
-        course.save()
-
-        return self.create_response(request, {})
+        return self.create_response(request, object_list)
