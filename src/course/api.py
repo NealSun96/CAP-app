982de41b3221624df36b82eae80bbe89de09b34a
Neal Sun <nealsun@Neals-MacBook-Pro.local>
2017-01-02 20:56:39 -0500

    add and edit endpoints

--- src/course/api.py
+++ src/course/api.py
@@ -1,8 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.conf.urls import url
+from django.contrib.auth.models import User
 from tastypie.authentication import ApiKeyAuthentication
 from tastypie.authorization import DjangoAuthorization
+from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.http import HttpBadRequest, HttpNotFound
 from tastypie.resources import ModelResource
-from tastypie.utils import trailing_slash
+from tastypie.utils import trailing_slash, dict_strip_unicode_keys
 
 from .models import Course
 from djgap.corsresource import CorsResourceBase
@@ -11,6 +15,7 @@ from djgap.corsresource import CorsResourceBase
 class CourseResource(CorsResourceBase, ModelResource):
     class Meta:
         queryset = Course.objects.all()
+        fields = ["id", "course_name", "start_time"]
         allowed_method = ['get']
         resource_name = 'course'
         authorization = DjangoAuthorization()
@@ -18,27 +23,60 @@ class CourseResource(CorsResourceBase, ModelResource):
 
     def prepend_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/courses%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_courses'), name="api_get_courses"),
+            url(r"^(?P<resource_name>%s)/add_course%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('add_course'), name="api_add_course"),
+            url(r"^(?P<resource_name>%s)/edit_course/(?P<id>\d+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('edit_course'), name="api_edit_course"),
         ]
 
-    def get_courses(self, request, **kwargs):
-        self.method_check(request, allowed=['get'])
+    def authorized_read_list(self, object_list, bundle):
+        return object_list.filter(teacher=bundle.request.user)
+
+    def add_course(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        self.is_authenticated(request)
+        user_group = request.user.groups.first().name
+        if user_group != "teacher":
+            raise ImmediateHttpResponse(HttpBadRequest('Not a teacher'))
+
+        deserialized = self.deserialize(request, request.body,
+                                        format=request.META.get('CONTENT_TYPE', 'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request, deserialized)
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
+
+        Course(course_name=bundle.data.get('course_name'), start_time=bundle.data.get('start_time'), teacher=bundle.request.user).save()
+
+        return self.create_response(request, {})
+
+    def edit_course(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
         self.is_authenticated(request)
 
-        # Do the query.
-        courses = Course.objects.filter(teacher=request.user)
+        user_group = request.user.groups.first().name
+        if user_group != "teacher":
+            raise ImmediateHttpResponse(HttpBadRequest('Not a teacher'))
 
-        objects = []
+        try:
+            course = Course.objects.get(id=kwargs['id'], teacher=request.user)
+        except ObjectDoesNotExist:
+            raise ImmediateHttpResponse(HttpNotFound('Course does not exist'))
 
-        for course in courses:
-            bundle = self.build_bundle(obj=course, request=request)
-            bundle.data['id'] = course.id
-            bundle.data['course_name'] = course.course_name
-            bundle.data['start_time'] = course.start_time
-            objects.append(bundle)
+        deserialized = self.deserialize(request, request.body,
+                                        format=request.META.get('CONTENT_TYPE', 'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request, deserialized)
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
 
-        object_list = {
-            'objects': objects,
-        }
+        if bundle.data.get('teacher') != bundle.request.user.username:
+            try:
+                other_teacher = User.objects.get(username=bundle.data.get('teacher'))
+                other_teacher_group = other_teacher.groups.first().name
+                if other_teacher_group != "teacher":
+                    raise ImmediateHttpResponse(HttpBadRequest('Assigned teacher is not a teacher'))
+            except ObjectDoesNotExist:
+                raise ImmediateHttpResponse(HttpNotFound('Assigned teacher does not exist'))
+            course.teacher = other_teacher
 
-        return self.create_response(request, object_list)
+        course.course_name = bundle.data.get('course_name')
+        course.start_time=bundle.data.get('start_time')
+        course.done = bundle.data.get('done')
+        course.save()
+
+        return self.create_response(request, {})
