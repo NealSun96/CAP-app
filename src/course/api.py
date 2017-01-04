725ecdbfefc49d158de00b343653b5c24caf3f4f
Neal Sun <nealsun@Neals-MacBook-Pro.local>
2017-01-03 21:01:17 -0500

    edit endpoint

--- src/course/api.py
+++ src/course/api.py
@@ -12,6 +12,10 @@ from tastypie.utils import trailing_slash, dict_strip_unicode_keys
 
 from .models import Course
 from employee_title.models import EmployeeTitle
+from action_plan.models import ActionPlan
+from knowledge_test.models import KnowledgeTest
+from question.models import Question
+from question_ordered.models import QuestionOrdered
 from djgap.corsresource import CorsResourceBase
 
 
@@ -29,6 +33,7 @@ class CourseResource(CorsResourceBase, ModelResource):
             url(r"^(?P<resource_name>%s)/add_course%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('add_course'), name="api_add_course"),
             url(r"^(?P<resource_name>%s)/edit_course/(?P<id>\d+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('edit_course'), name="api_edit_course"),
             url(r"^(?P<resource_name>%s)/get_assignments/(?P<id>\d+)/(?P<type>\w+)/(?P<level>[\w-]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('get_assignments'), name="api_get_assignments"),
+            url(r"^(?P<resource_name>%s)/edit_assignments/(?P<id>\d+)/(?P<type>\w+)/(?P<level>[\w-]+)%s$" % (self._meta.resource_name, trailing_slash()), self.wrap_view('edit_assignments'), name="api_edit_assignments"),
         ]
 
     def authorized_read_list(self, object_list, bundle):
@@ -106,25 +111,25 @@ class CourseResource(CorsResourceBase, ModelResource):
         objects = []
         if a_type == 'action_plan':
             if len(course.actionplan_set.filter(level=a_level).all()) <= 0:
-                raise ImmediateHttpResponse(HttpNotFound('Action plan does not exist'))
-
-            action_plan = course.actionplan_set.filter(level=a_level).first()
-            bundle = self.build_bundle(obj=action_plan, request=request)
-            bundle.data['action_points'] = json.loads(action_plan.action_points)
+                bundle = self.build_bundle(request=request)
+            else:
+                action_plan = course.actionplan_set.filter(level=a_level).first()
+                bundle = self.build_bundle(obj=action_plan, request=request)
+                bundle.data['action_points'] = json.loads(action_plan.action_points)
             objects.append(bundle)
         elif a_type == 'knowledge_test':
             if len(course.knowledgetest_set.filter(level=a_level).all()) <= 0:
-                raise ImmediateHttpResponse(HttpNotFound('Knowledge test does not exist'))
-
-            knowledge_test = course.knowledgetest_set.filter(level=a_level).first()
-            bundle = self.build_bundle(obj=knowledge_test, request=request)
-            bundle.data['questions'] = [{
-                'ordering': question.question.ordering,
-                'question': question.question.question_body,
-                'answer_keys': [answer for answer in json.loads(question.question.answer_keys)],
-                'right_answer': question.question.right_answer,
-                'score': question.score
-            } for question in knowledge_test.questionordered_set.all()]
+                bundle = self.build_bundle(request=request)
+            else:
+                knowledge_test = course.knowledgetest_set.filter(level=a_level).first()
+                bundle = self.build_bundle(obj=knowledge_test, request=request)
+                bundle.data['questions'] = [{
+                    'ordering': question.ordering,
+                    'question': question.question.question_body,
+                    'answer_keys': [answer for answer in json.loads(question.question.answer_keys)],
+                    'right_answer': question.question.right_answer,
+                    'score': question.score
+                } for question in knowledge_test.questionordered_set.all()]
             objects.append(bundle)
 
         object_list = {
@@ -132,3 +137,76 @@ class CourseResource(CorsResourceBase, ModelResource):
         }
 
         return self.create_response(request, object_list)
+
+    def edit_assignments(self, request, **kwargs):
+        self.method_check(request, allowed=['post'])
+        self.is_authenticated(request)
+
+        user_group = request.user.groups.first().name
+        if user_group != "teacher":
+            raise ImmediateHttpResponse(HttpBadRequest('Not a teacher'))
+
+        try:
+            course = Course.objects.get(id=kwargs['id'], teacher=request.user)
+        except ObjectDoesNotExist:
+            raise ImmediateHttpResponse(HttpNotFound('Course does not exist'))
+
+        deserialized = self.deserialize(request, request.body,
+                                        format=request.META.get('CONTENT_TYPE', 'application/json'))
+        deserialized = self.alter_deserialized_detail_data(request, deserialized)
+        bundle = self.build_bundle(data=dict_strip_unicode_keys(deserialized), request=request)
+
+        accepted_levels = set(EmployeeTitle.TITLE_PERMS.keys()) - {EmployeeTitle.TITLE_TEACHER, EmployeeTitle.TITLE_UNKNOWN}
+        a_level = kwargs['level']
+        if a_level not in accepted_levels:
+            raise ImmediateHttpResponse(HttpBadRequest('Wrong assignment level'))
+
+        a_type = kwargs['type']
+        if a_type not in ['action_plan', 'knowledge_test']:
+            raise ImmediateHttpResponse(HttpBadRequest('Wrong assignment type'))
+
+        if a_type == 'action_plan':
+            if len(bundle.data.get('action_points')) != len(set(bundle.data.get('action_points'))):
+                raise ImmediateHttpResponse(HttpBadRequest('Duplicate action points'))
+            try:
+                action_plan = ActionPlan.objects.get(course=course, level=a_level)
+                action_plan.action_points = bundle.data.get('action_points')
+                action_plan.save()
+            except ActionPlan.DoesNotExist:
+                ActionPlan(course=course, level=a_level, action_points=bundle.data.get('action_points')).save()
+        elif a_type == 'knowledge_test':
+            try:
+                knowledge_test = KnowledgeTest.objects.get(course=course, level=a_level)
+            except KnowledgeTest.DoesNotExist:
+                knowledge_test = KnowledgeTest(course=course, level=a_level, time_span=0)
+                knowledge_test.save()
+            post_questions = json.loads(bundle.data.get('questions'))
+            questions = knowledge_test.questionordered_set.all()
+            for q, post_q in zip(questions, post_questions):
+                q.score = post_q.get('score')
+                q.question.question_body = post_q.get('question_body')
+                answer_keys = post_q.get('answer_keys')
+                if len(answer_keys) != len(set(answer_keys)):
+                    raise ImmediateHttpResponse(HttpBadRequest('Duplicate answer keys in %s' % post_q.get('question_body')))
+                q.question.answer_keys = json.dumps(answer_keys)
+                q.question.right_answer = post_q.get('right_answer')
+                q.question.save()
+                q.save()
+            ordering = len(questions)
+            for post_q in post_questions[len(questions):]:
+                answer_keys = post_q.get('answer_keys')
+                if len(answer_keys) != len(set(answer_keys)):
+                    raise ImmediateHttpResponse(HttpBadRequest('Duplicate answer keys in %s' % post_q.get('question_body')))
+                new_question = Question(question_body=post_q.get('question_body'),
+                                        answer_keys=json.dumps(answer_keys),
+                                        right_answer=post_q.get('right_answer'))
+                new_question.save()
+                new_question_ordered = QuestionOrdered(ordering=ordering, test=knowledge_test,
+                                                       question=new_question, score=post_q.get('score'))
+                new_question_ordered.save()
+                ordering += 1
+            for q in questions[len(post_questions):]:
+                q.delete()
+                q.question.delete()
+
+        return self.create_response(request, {})
