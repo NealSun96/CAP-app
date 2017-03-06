6aa976c74e48e985ace075031300340d5d1d2ab2
kalindu01 <kalindu.decosta@mail.utoronto.ca>
2017-03-05 22:35:26 -0500

    added a register page

--- src/static/djgap/js/main.js
+++ src/static/djgap/js/main.js
@@ -1,34 +1,58 @@
 
 $(document).ready(function() {
-$("#submit").click(function() {
-    var username = document.forms["login"]["username"].value;
-    var password = document.forms["login"]["password"].value;
-    var encodedString = btoa(username + ":" + password)
-    var baseUrl = getUrl();
-    var endPoint = baseUrl + "/api/v1/login/";
-    $.ajax({
-        type: "GET",
-        url: endPoint,
-        data: {},
-        success: function(data){
-            var auth = username + ":" + data.objects[0].api_key;
-            setTimeout(function() {window.location.href=baseUrl+"/courses/" + btoa(auth);});
-        },
-        error: function(data){
-            error();
-        },
-        beforeSend: function(xhr){
-            xhr.setRequestHeader("Authorization", "Basic " + encodedString);
-            xhr.setRequestHeader("Content-Type", "application/json");
-        },
-        complete: function(){
-        }
-    })
-    $('#login').submit();
-});
+    $('.invalid').click(function(){clearError();});
+    
+    $("#submit").click(function() {
+        var username = document.forms["login"]["username"].value;
+        var password = document.forms["login"]["password"].value;
+        var encodedString = btoa(username + ":" + password)
+        var baseUrl = getUrl();
+        var endPoint = baseUrl + "/api/v1/login/";
+        $.ajax({
+            type: "GET",
+            url: endPoint,
+            data: {},
+            success: function(data){
+                var auth = username + ":" + data.objects[0].api_key;
+                setTimeout(function() {window.location.href=baseUrl+"/courses/" + btoa(auth);});
+            },
+            error: function(data){
+                error();
+            },
+            beforeSend: function(xhr){
+                xhr.setRequestHeader("Authorization", "Basic " + encodedString);
+                xhr.setRequestHeader("Content-Type", "application/json");
+            },
+            complete: function(){
+            }
+        })
+        $('#login').submit();
+    });
+    
+    $("#login #register").click(function(){
+        clearError();
+        $('#login').addClass("hidden");
+        $('#registerForm').removeClass("hidden");
+    });
+    
+    $("#registerForm #regBtn").click(function(){
+        registerUser();
+        $('#login').removeClass("hidden");
+        $('#registerForm').addClass("hidden");
+    });
 
 });
 
+
+function registerUser() {
+    
+    // do register stuff
+}
+
+function clearError() {
+    $('.invalid').fadeOut(100);
+    
+}
 
 function error() {
     $('.invalid').fadeIn(400);
