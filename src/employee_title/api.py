da4e60a07cfb5afdd752adc0d1c2921cd1bcc448
Neal Sun <nealsun@Neals-MacBook-Pro.local>
2017-05-08 11:32:21 -0400

    rstrip

--- src/employee_title/api.py
+++ src/employee_title/api.py
@@ -50,7 +50,7 @@ class EmployeeTitleResource(CorsResourceBase, ModelResource):
         else:
             raise ImmediateHttpResponse(HttpBadRequest('该文件无法被阅读'))
         rows = file_data.split("\n")
-        rows = [x for x in rows if x != ""]
+        rows = [x.rstrip() for x in rows if x != ""]
         for row in rows:
             if EmployeeTitle.objects.filter(username=row, title=EmployeeTitle.TITLE_TEACHER).count() == 0:
                 EmployeeTitle(username=row, title=EmployeeTitle.TITLE_TEACHER).save()
