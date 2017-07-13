bbf57246acf64bdcb6c02b1e429b380a177eec55
Neal Sun <nealsun@Neals-MacBook-Pro.local>
2017-07-12 21:33:49 -0700

    astimezone

--- src/enrollment/api.py
+++ src/enrollment/api.py
@@ -75,7 +75,7 @@ class EnrollmentResource(CorsResourceBase, ModelResource):
             current_date = datetime.now(pytz.timezone('Asia/Shanghai'))
             knowledge_test_open_date = (enrollment.start_time
                                        + timedelta(enrollment.course.KNOWLEDGE_TEST_OPEN_DAYS)).\
-                replace(tzinfo=pytz.timezone('Asia/Shanghai'))
+                astimezone(pytz.timezone('Asia/Shanghai'))
             knowledge_test_open_string = knowledge_test_open_date.strftime(enrollment.OPEN_DATE_FORMAT)
             has_knowledge_test = enrollment.course.knowledgetest_set.count() > 0
             has_knowledge_test_answer = enrollment.knowledgetestanswer_set.count() > 0
@@ -89,7 +89,7 @@ class EnrollmentResource(CorsResourceBase, ModelResource):
                 bundle.data['knowledge_test_status'], bundle.data['knowledge_test_color'] = '开放于%s' % knowledge_test_open_string, "{color: 'black'}"
 
             diagnosis_open_date = (enrollment.start_time + timedelta(enrollment.course.DIAGNOSIS_OPEN_DAYS)).\
-                replace(tzinfo=pytz.timezone('Asia/Shanghai'))
+                astimezone(pytz.timezone('Asia/Shanghai'))
             diagnosis_open_string = diagnosis_open_date.strftime(enrollment.OPEN_DATE_FORMAT)
             has_diagnosis = enrollment.diagnosis_set.count() > 0
             if has_diagnosis:
